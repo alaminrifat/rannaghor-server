5f25239151e50d7ad96b7180669bbc48f9b10dfb
alaminrifat <alaminrifat71@gmail.com>
2023-05-01 21:00:40 +0600

    cors middleware setup with test

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
+var cors = require('cors')
 const port = 5000
 
+app.use(cors())
+
 app.get('/', (req, res) => {
-  res.send('Chef Recipe Hunter Server is Running')
+  res.send('Chef Recipe Hunter Server is Running...')
 })
 
 app.listen(port, () => {
