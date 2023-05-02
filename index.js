5d30769790cbbb76c02afd41179bc3444a9bad39
alaminrifat <alaminrifat71@gmail.com>
2023-05-03 00:04:41 +0600

    chef data get

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const app = express()
 var cors = require('cors')
 const port = 5000
 
+const chef_data = require('./Data/data.json')
 app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Chef Recipe Hunter Server is Running...')
+})
+app.get('/chef', (req, res) => {
+  res.send(chef_data);
 })
 
 app.listen(port, () => {
